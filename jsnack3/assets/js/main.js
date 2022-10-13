477012dee863f401d8dcfd70912bae29d1ac705b
Salano24 <salernomarco48@email.com>
2022-10-13 12:03:15 +0200

    ciclo while

--- jsnack3/assets/js/main.js
+++ jsnack3/assets/js/main.js
@@ -1,5 +1,5 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-let sommaNumeri = 0
+/* let sommaNumeri = 0
 for (let index = 0; index < 10; index++) {
     
 
@@ -8,9 +8,23 @@ for (let index = 0; index < 10; index++) {
     
     sommaNumeri += numeri
 
-}
+} */
 
 //Il programma stampa la somma di tutti i numeri inseriti. (modificato) 
  
+
+/* console.log(sommaNumeri); */
+
+/* utilizzo il ciclo while */
+let index = 0
+let sommaNumeri = 0
+
+while (index < 10){
+    const numeri = Number(prompt('Inserisci un numero'))
+
+     index++
+    sommaNumeri += numeri
+
+}
 
 console.log(sommaNumeri);
