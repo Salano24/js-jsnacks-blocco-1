8fb6655eb376867f5982244526d4a57336974090
Salano24 <salernomarco48@email.com>
2022-10-13 12:32:43 +0200

    fine esercizio

---
+++ jsnack4/assets/js/main.js
@@ -0,0 +1,16 @@
+//Chiedi all’utente il suo nome,
+const nomeUtente = prompt('Inserisci il tuo nome')
+const listaInvitati = ['Fabio Pacifici', 'Marco Salerno']
+//controlla che sia nella lista di chi può accedere,
+let numeroInvitati= 0
+
+for(let i = 0; i < listaInvitati.length; i++){
+    if(listaInvitati.includes(emailUtente) == false){
+        numeroInvitati++
+    }
+}
+//stampa un messaggio appropriato sull’esito del controllo.
+if(numeroInvitati == listaInvitati.length){
+    alert(`Non sei tra gli invitati`)
+} else {
+    alert(`Benvenuto ${nomeUtente}`)
